28ff10d4d9afeef8199f1f9de934b701b82f6f91
simehaa <simen.haapnes@gmail.com>
2018-02-19 16:09:09 +0100

    19 feb

--- 4semester/fys2140/oblig3_b.py
+++ 4semester/fys2140/oblig3_b.py
@@ -3,8 +3,8 @@ from math import sqrt
 import matplotlib.pyplot as plt
 
 k = np.linspace(-5,5,1001)
-x = np.linspace(0,20,1001)
-a = 0.5
+x = np.linspace(-10,10,1001)
+a = 1
 A = a/(k**2 + a**2)
 y = A*np.sin(x*k)
 
